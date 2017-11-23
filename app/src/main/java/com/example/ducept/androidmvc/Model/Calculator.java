9f0a35942ad88bde5ef0e63190e5f752364fea16
AntidotRepository <haroldducept@gmail.com>
2017-11-23 21:11:59 +0100

    Added Calculator to model

---
+++ app/src/main/java/com/example/ducept/androidmvc/Model/Calculator.java
@@ -0,0 +1,92 @@
+package com.example.ducept.androidmvc.Model;
+import com.example.ducept.androidmvc.Model.Observable;
+
+/**
+ * Created by ducept on 23/11/2017.
+ */
+
+public class Calculator extends AbstractModel
+{
+    // Define operator
+    public void setOperator(String ope)
+    {
+        // Launch calculation
+        calculate();
+
+        // Store the operator
+        this.operator = ope;
+
+        // If operator isn't "="
+        if(!ope.equals("="))
+        {
+            // We reinitialize operand
+            this.operand = "";
+        }
+    }
+
+    // Define the number
+    public void setNumber(String result)
+    {
+        // We concatenate the number
+        this.operand += result;
+        // We update
+        notifyObserver(this.operand);
+    }
+
+    // Force calculation
+    public void getResult()
+    {
+        calculate();
+    }
+
+    // We re-init everything
+    public void reset()
+    {
+        this.result = 0;
+        this.operand = "0";
+        this.operator = "";
+
+        // Update
+        notifyObserver(String.valueOf(this.result));
+    }
+
+    // Calculation
+    public void calculation()
+    {
+        // If there is no operator, the result is typed
+        if(this.operator.equals(""))
+        {
+            this.result = Double.parseDouble(this.operand);
+        }
+        else
+        {
+            // If the operand is not empty, we calculate with it
+            if(!this.operator.equals(""))
+            {
+                if(this.operator.equals("+"))
+                {
+                    this.result += Double.parseDouble(this.operand);
+                }
+                if(this.operator.equals("-"))
+                {
+                    this.result -= Double.parseDouble(this.operand);
+                }
+                if(this.operator.equals("*"))
+                {
+                    this.result *= Double.parseDouble(this.operand);
+                }
+                if(this.operator.equals("/"))
+                {
+                    try
+                    {
+                        this.result /= Double.parseDouble(this.operand);
+                    }
+                    catch (ArithmeticException e)
+                    {
+                        this.result = 0;
+                    }
+                }
+            }
+        }
+    }
+}
