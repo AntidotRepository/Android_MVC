b2fc977037668ca95ce1c9c38e66b6c6ff84ccde
AntidotRepository <haroldducept@gmail.com>
2017-12-25 15:22:25 +0100

    Link textView in XML and in java

--- app/src/main/java/com/example/ducept/androidmvc/MainActivity.java
+++ app/src/main/java/com/example/ducept/androidmvc/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ducept.androidmvc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView tv_result = (TextView)findViewById(R.id.tv_result);
+        tv_result.setText("Salut");
+        tv_result.setTextSize(30);
+
+
     }
 }
